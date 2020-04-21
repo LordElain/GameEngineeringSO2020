31e32d305fac54212a07d4a92a6f157947687bc6
Kevin Elain <47543964+LordElain@users.noreply.github.com>
2020-04-21 05:16:49 +0200

    Übung 1.2 fertig
    Übung 1.2
    Assets hinzugefügt

---
+++ Game Engineering - Auto Flipper/Assets/InitialisererGOs.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialisererGOs : MonoBehaviour
+{
+    public GameObject m_KugelPrefab;
+    public Transform m_SpawnKugelTransform;
+    public float m_lifetime = 3.0f;
+
+    private GameObject aKugel;
+    private float m_timesincestart = 0;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        aKugel = Instantiate(m_KugelPrefab, m_SpawnKugelTransform);
+        aKugel.name = "Kugel";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_timesincestart += Time.deltaTime;
+        Debug.Log("Time: " + m_timesincestart);
+   
+        if(m_timesincestart > m_lifetime)
+        {
+            Destroy(aKugel);
+        }
+    }
+}
---
+++ Game Engineering - Auto Flipper/Assets/TestStandardFunctions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestStandardFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        Debug.Log("Awake called");
+    }
+    void Start()
+    {
+        Debug.Log("Start called");
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log("FixedUpdate called");
+    }
+
+    private void LateUpdate()
+    {
+        Debug.Log("LateUpdate called");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update called");
+    }
+}
